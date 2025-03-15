bcd99da428bff0667285caf074169ebb0418cd75
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-03-16 01:44:27 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
+++ Python/백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
@@ -1,22 +1,17 @@
-# 17140번 이차원 배열과 연산
-
-# R 연산 : 행의 개수 >= 열의 개수인 경우 적용
-def R_cal(arr):
+# 문제에서 제시한 정렬 연산
+def sort_cal(arr):
     max_row = 0
 
     for i in range(len(arr)):
         temp = []
         for n in set(arr[i]):
-            if n == 0:
+            if n == 0:	# 0은 제외
                 continue
             temp.append([n, arr[i].count(n)])
-        arr[i] = temp
-        arr[i] = sorted(arr[i],key = lambda x : (x[1], x[0]))
-        arr[i] = sum(arr[i],[])
+        temp = sorted(temp, key = lambda x : (x[1], x[0]))
+        temp = sum(temp, [])
+        arr[i] = temp[:100]	
         max_row = max(max_row, len(arr[i]))
-        
-    if max_row > 100:
-        max_row = 100
 
     for i in range(len(arr)):
         if len(arr[i]) != max_row:
@@ -24,54 +19,30 @@ def R_cal(arr):
 
     return arr
 
-# C 연산 : 행의 개수 < 열의 개수인 경우 적용
-def C_cal(arr):
-    arr = list(zip(*arr))
-    temp = []
-    max_col = 0
-
-    for i in range(len(arr)):
-        temp = []
-        for n in set(arr[i]):
-            if n == 0:
-                continue
-            temp.append([n, arr[i].count(n)])
-        arr[i] = temp
-        arr[i] = sorted(arr[i],key = lambda x : (x[1], x[0]))
-        arr[i] = sum(arr[i],[])
-        max_col = max(max_col, len(arr[i]))
-        
-    if max_col > 100:
-        max_col = 100
-
-    for i in range(len(arr)):
-        if len(arr[i]) != max_col:
-            arr[i] = arr[i] + [0] * (max_col - len(arr[i]))
-
-    arr = list(zip(*arr))
-    return arr
-
 r, c, k = map(int,input().split())
 A = [list(map(int,input().split())) for _ in range(3)]
 
-cnt = 0
+sec = 0 
 while True:
     row, col = len(A), len(A[0])
-
+	
+    # A[r][c]가 k일 경우의 연산
     if row >= r and col >= c and A[r - 1][c - 1] == k:
-        print(cnt)
+        print(sec)
         break
-
-    if cnt > 100:
+	
+    # 100초가 지났을 경우의 연산
+    if sec > 100:
         print(-1)
-        break
-    
+        break    
     
     # 행의 개수 >= 열의 개수인 경우, R연산(R_cal)
     if row >= col:
-        A = R_cal(A)
+        A = sort_cal(A)
     # 행의 개수 < 열의 개수인 경우, C연산(C_cal)
     else:
-        A = C_cal(A)
-    cnt += 1
+        A = list(zip(*A))
+        A = sort_cal(A)
+        A = list(zip(*A))	# 전환한 배열 되돌리기
     
+    sec += 1
