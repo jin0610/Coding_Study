9ac55dce9fc195bbac202baf78710b1135bc61c3
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-04-08 00:06:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 32488 KB -BaekjoonHub

--- Python/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ Python/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,28 +1,33 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-maps = [list(map(int, list(input()))) for _ in range(N)]
+# sys.stdin.readline를 할 경우 개행 문자('\n')이 들어가므로 제거해야함
+apart_map = [list(map(int, list(input())[:-1])) for _ in range(N)] 
 
-dx, dy = [0, 0, 1, -1], [ -1, 1, 0, 0]
-
+dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0] # 상하좌우
+# dfs 방식
 def dfs(x, y):
-    if x >= 0 and x < N and y >= 0 and y < N and maps[y][x] == 1:
+    if x >= 0 and x < N and y >= 0 and y <= N:
         global cnt
-        cnt += 1
-        maps[y][x] = 0
+        visited[y][x] = True
         for d in range(4):
             nx, ny = x + dx[d], y + dy[d]
-            dfs(nx, ny)
-    return cnt
+            if nx >= 0 and nx < N and ny >= 0 and ny < N and not visited[ny][nx] and apart_map[ny][nx] == 1:
+                visited[ny][nx] = 1
+                cnt += 1
+                dfs(nx, ny)
 
-cnt = 0
-apartment = []
-for x in range(N):
-    for y in range(N):
-        if maps[y][x] == 1:
-            count = dfs(x, y)
-            apartment.append(count)
-            cnt = 0 
+apart_cnt = []
+visited = [[False] * N for _ in range(N)]
+for y in range(N):
+    for x in range(N):
+        if apart_map[y][x] == 1 and not visited[y][x]:
+            cnt = 1
+            dfs(x, y)
+            apart_cnt.append(cnt)
 
-print(len(apartment))
-apartment.sort()
-for i in range(len(apartment)):
-    print(apartment[i])
+apart_cnt.sort()
+print(len(apart_cnt))
+for i in range(len(apart_cnt)):
+    print(apart_cnt[i])
