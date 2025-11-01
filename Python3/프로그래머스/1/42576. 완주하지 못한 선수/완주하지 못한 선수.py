5ba2227d37047ce51ddfb23b0d561553dee2c73a
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-11-01 09:00:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.89 ms, Memory: 36.7 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -4,10 +4,11 @@ def solution(participant, completion):
     sumHash = 0
     
     for p in participant:
-        hashDict[hash(p)] = p  # hash 인덱스 넣기
+        hashDict[hash(p)] = p    # hash 인덱스 넣기
         sumHash += hash(p) # hash 총합
-    
+        
     for c in completion:
         sumHash -= hash(c)
-        
-    return hashDict[sumHash]
+    
+    answer = hashDict[sumHash]
+    return answer
