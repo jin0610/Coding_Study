140ced00f64ce9fd328c4f6ab872a612726812d2
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-11-01 09:47:47 +0900

    [level 2] Title: 모음 사전, Time: 3.99 ms, Memory: 9.49 MB -BaekjoonHub

--- Python3/프로그래머스/2/84512. 모음 사전/README.md
+++ Python3/프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 0.0 MB, 시간: 0.00 ms
+메모리: 9.49 MB, 시간: 3.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 02일 08:04:21
+2025년 11월 01일 09:47:48
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,13 +1,31 @@
+words = ['A', 'E', 'I', 'O', 'U']
+words_comb = []
+dictionary = []
+
+def comb(cnt, w):
+    global words, words_comb, dictionary
+    
+    if cnt == w:
+        dictionary.append(''.join(words_comb))
+        return
+    
+    for i in range(5):
+        words_comb.append(words[i])
+        comb(cnt + 1, w)
+        words_comb.pop()
+                          
+    return
+
 def solution(word):
+    global words, words_comb, dictionary
     answer = 0
-    dictionary = []
-    words = 'AEIOU'
-    def dfs(cnt, w):
-        if cnt == 5:
-            return
-        for i in range(len(words)):
-            dictionary.append(w + words[i])
-            dfs(cnt + 1, w + words[i])
-    dfs(0,'')
+    
+    for i in range(1, 6):
+        comb(0, i)
+    
+    dictionary = sorted(dictionary)
     answer = dictionary.index(word) + 1
+        
+    
+
     return answer
