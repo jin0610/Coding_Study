8ca0986654f26771ff363eedfc83e910d84fff6d
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-12 00:38:44 +0900

    [level 2] Title: 타겟 넘버, Time: 742.51 ms, Memory: 9.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,26 +1,24 @@
+#### backtracking ####
 selected = []
 answer = 0
-original = []
-def backtracking(curr_n, idx, target):
-    global selected, answer, original
-    if curr_n == len(original):
+def backtracking(curr_n, idx, numbers, target):
+    global selected, answer
+    
+    if curr_n == len(numbers):
         if sum(selected) == target:
             answer += 1
         return
     
+    selected.append(numbers[idx])
+    backtracking(curr_n + 1, idx + 1, numbers, target)
+    selected.pop()
     
-    selected.append(original[idx])
-    backtracking(curr_n + 1, idx + 1, target)
+    selected.append(-1 * numbers[idx])
+    backtracking(curr_n + 1, idx + 1, numbers, target)
     selected.pop()
-
-    selected.append(-1 * original[idx])
-    backtracking(curr_n + 1, idx + 1, target)
-    selected.pop()
-
+    
 def solution(numbers, target):
-    global answer, original
-    original = numbers.copy()
-    
-    backtracking(0, 0, target)
+    global answer
+    backtracking(0, 0, numbers, target)
     
     return answer
