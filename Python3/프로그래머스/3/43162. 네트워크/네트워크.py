d5cffafecc9d7b7c1216d79e5990f38349eaf6bf
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-12 01:16:02 +0900

    [level 3] Title: 네트워크, Time: 0.89 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,21 @@
+visited = []
+def dfs(v, n, computers):
+    global visited
+    
+    for i in range(n):
+        if computers[v][i] == 1 and not visited[i]:
+            visited[v] = True
+            dfs(i, n, computers)
+    
+def solution(n, computers):
+    global visited
+    visited = [False for _ in range(n)]
+    answer = 0
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+
+            dfs(i, n, computers)
+            answer += 1
+    
+    return answer
